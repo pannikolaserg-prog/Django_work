4b148522971e68d4546c09cd694465e7a720bbf0
Nikolay Panov <pannikolaserg@gmail.com>
2026-04-12 23:40:26 +0900

    add_products.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,5 +11,5 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "category", "price"]
-    list_filter = ["category",]
+    list_filter = ["category", ]
     search_fields = ["name", "description"]
---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = "Add product to the database"
+
+    def handle(self, *args, **options):
+        category, _ = Category.objects.get_or_create(
+            name="Овощи", description="Свежие овощи"
+        )
+
+        products = [
+            {
+                "name": "Огурец",
+                "description": "Не фрукт",
+                "price": "50",
+                "category": category,
+            },
+            {
+                "name": "Помидор",
+                "description": "Овощ",
+                "price": "50",
+                "category": category,
+            },
+        ]
+
+        for product_data in products:
+            product, created = Product.objects.get_or_create(**product_data)
+            if created:
+                self.stdout.write(
+                    self.style.SUCCESS(f"Successfully added product: {product.name}")
+                )
+            else:
+                self.stdout.write(
+                    self.style.WARNING(f"Product already exist: {product.name}")
+                )
