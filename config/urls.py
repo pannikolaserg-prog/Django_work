7f9e0e92fc922031c4998ac515daaea900ace95d
Nikolay Panov <pannikolaserg@gmail.com>
2026-04-13 21:17:08 +0900

    add debug in urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Добавляем маршруты для медиафайлов ТОЛЬКО в режиме DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
