143df2734c239c4663ab7e3a42320615b936b247
Nikolay Panov <pannikolaserg@gmail.com>
2026-05-11 01:01:09 +0900

    correcting urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,72 +1,42 @@
-from django.contrib import messages
-from django.views.generic import ListView, DetailView, TemplateView, FormView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from .forms import ContactForm, ProductForm
-from .models import Product
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from catalog.forms import ProductForm
+from catalog.models import Product
 
 
-class HomeView(TemplateView):
-    template_name = "products/home.html"
-
-
-class ContactView(FormView):
-    template_name = "products/contacts.html"
-    form_class = ContactForm
-    success_url = reverse_lazy("catalog:contacts")
-
-    def form_valid(self, form):
-        messages.success(self.request, f"{form.cleaned_data['name']}, ваше сообщение отправлено!")
-        return super().form_valid(form)
-
-
-class ProductListView(ListView):
+class ProductListView(ListView):  # Общедоступный
     model = Product
-    template_name = "products/product_list.html"
+    template_name = "catalog/product_list.html"
     context_object_name = "products"
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):  # Только для авторизованных
     model = Product
-    template_name = "products/product_detail.html"
+    template_name = "catalog/product_detail.html"
     context_object_name = "product"
+    login_url = 'users:login'
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):  # Только для авторизованных
     model = Product
-    form_class = ProductForm  # Используем форму с валидацией вместо fields
-    template_name = "products/product_form.html"
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
     success_url = reverse_lazy('catalog:product_list')
-
-    def form_valid(self, form):
-        messages.success(self.request, "Продукт успешно создан!")
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "Исправьте ошибки в форме")
-        return super().form_invalid(form)
+    login_url = 'users:login'
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):  # Только для авторизованных
     model = Product
-    form_class = ProductForm  # Используем форму с валидацией
-    template_name = "products/product_form.html"
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
     success_url = reverse_lazy('catalog:product_list')
-
-    def form_valid(self, form):
-        messages.success(self.request, "Продукт успешно обновлен!")
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "Исправьте ошибки в форме")
-        return super().form_invalid(form)
+    login_url = 'users:login'
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):  # Только для авторизованных
     model = Product
-    template_name = "products/product_confirm_delete.html"
+    template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy('catalog:product_list')
-
-    def delete(self, request, *args, **kwargs):
-        messages.success(self.request, "Продукт успешно удален!")
-        return super().delete(request, *args, **kwargs)
+    login_url = 'users:login'
