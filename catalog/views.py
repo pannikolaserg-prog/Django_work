ed6f607c4ea26cfc5d92a32632758446a9946c93
Nikolay Panov <pannikolaserg@gmail.com>
2026-05-11 10:50:12 +0900

    finish correcting

--- catalog/views.py
+++ catalog/views.py
@@ -1,42 +1,77 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, TemplateView, FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin  # Добавьте для авторизации
+from .forms import ContactForm, ProductForm
+from .models import Product
 
-from catalog.forms import ProductForm
-from catalog.models import Product
+
+class HomeView(TemplateView):
+    template_name = "products/home.html"
 
 
-class ProductListView(ListView):  # Общедоступный
+class ContactView(FormView):
+    template_name = "products/contacts.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("catalog:contacts")
+
+    def form_valid(self, form):
+        messages.success(self.request, f"{form.cleaned_data['name']}, ваше сообщение отправлено!")
+        return super().form_valid(form)
+
+
+class ProductListView(ListView):
     model = Product
-    template_name = "catalog/product_list.html"
+    template_name = "products/product_list.html"
     context_object_name = "products"
 
 
-class ProductDetailView(LoginRequiredMixin, DetailView):  # Только для авторизованных
+class ProductDetailView(LoginRequiredMixin, DetailView):  # 🔥 Добавлен LoginRequiredMixin
     model = Product
-    template_name = "catalog/product_detail.html"
+    template_name = "products/product_detail.html"
     context_object_name = "product"
-    login_url = 'users:login'
+    login_url = 'users:login'  # 🔥 Добавлено для редиректа
 
 
-class ProductCreateView(LoginRequiredMixin, CreateView):  # Только для авторизованных
+class ProductCreateView(LoginRequiredMixin, CreateView):  # 🔥 Добавлен LoginRequiredMixin
     model = Product
     form_class = ProductForm
-    template_name = "catalog/product_form.html"
+    template_name = "products/product_form.html"
     success_url = reverse_lazy('catalog:product_list')
-    login_url = 'users:login'
+    login_url = 'users:login'  # 🔥 Добавлено для редиректа
+
+    def form_valid(self, form):
+        messages.success(self.request, "Продукт успешно создан!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Исправьте ошибки в форме")
+        return super().form_invalid(form)
 
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):  # Только для авторизованных
+class ProductUpdateView(LoginRequiredMixin, UpdateView):  # 🔥 Добавлен LoginRequiredMixin
     model = Product
     form_class = ProductForm
-    template_name = "catalog/product_form.html"
+    template_name = "products/product_form.html"
     success_url = reverse_lazy('catalog:product_list')
-    login_url = 'users:login'
+    login_url = 'users:login'  # 🔥 Добавлено для редиректа
+
+    def form_valid(self, form):
+        messages.success(self.request, "Продукт успешно обновлен!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Исправьте ошибки в форме")
+        return super().form_invalid(form)
 
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):  # Только для авторизованных
+class ProductDeleteView(LoginRequiredMixin, DeleteView):  # 🔥 Добавлен LoginRequiredMixin
     model = Product
-    template_name = "catalog/product_confirm_delete.html"
+    template_name = "products/product_confirm_delete.html"
     success_url = reverse_lazy('catalog:product_list')
-    login_url = 'users:login'
+    login_url = 'users:login'  # 🔥 Добавлено для редиректа
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Продукт успешно удален!")
+        return super().delete(request, *args, **kwargs)
--- users/models.py
+++ users/models.py
@@ -1,16 +1,39 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """Кастомный менеджер пользователей без username"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
     """Кастомная модель пользователя с email в качестве логина"""
 
-    # Убираем username, делаем email обязательным
     username = None
     email = models.EmailField(_('email address'), unique=True)
 
-    # Дополнительные поля
     avatar = models.ImageField(
         upload_to='avatars/',
         blank=True,
@@ -30,9 +53,11 @@ class User(AbstractUser):
         verbose_name='Страна'
     )
 
-    # Используем email для авторизации
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']  # Поля, обязательные при создании суперпользователя
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # Используем кастомный менеджер
+    objects = UserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
