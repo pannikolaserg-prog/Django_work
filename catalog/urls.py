5e782673d34548771a48522535096ee7811b6be2
Nikolay Panov <pannikolaserg@gmail.com>
2026-05-15 22:39:25 +0900

    add correcting to catalog/urls.py and product_list.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('products/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
     path('contacts/', views.ContactView.as_view(), name='contacts'),
+    path('products/<int:pk>/publish-toggle/', views.ProductPublishToggleView.as_view(), name='product_publish_toggle'),
 ]
