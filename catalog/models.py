3027550e4182a49a8a9337aa9b30a3c725031ee3
Nikolay Panov <pannikolaserg@gmail.com>
2026-05-15 22:23:19 +0900

    add is_published in Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class TimeStampedModel(models.Model):
@@ -32,9 +35,22 @@ class Product(TimeStampedModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
 
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Владелец',
+        related_name='products',
+        null=True,
+        blank=True
+    )
+
     class Meta:
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
+        permissions = [
+            ('can_unpublish_product', 'Может отменять публикацию продукта'),
+        ]
 
     def __str__(self):
         return self.name
